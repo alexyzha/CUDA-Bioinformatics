4844562c1162819c647652eff8b4fabafe629308
aly <alexyzha@gmail.com>
2025-03-23 14:15:50 -0700

    cuda enabled docker image + nvidia ctk config for linux env

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 ## Ubuntu base image
-FROM ubuntu:22.04
+FROM nvidia/cuda:12.1.1-devel-ubuntu22.04
 ENV DEBIAN_FRONTEND=noninteractive
 
 ## Install basic
--- tests/helloworld.cu
+++ tests/helloworld.cu
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-__global__ hello() {
+__global__ void hello() {
     printf("B: %u, T: %u\n",blockIdx.x,threadIdx.x);
 }
 
-int main(int argc, char argv[]) {
+int main(int argc, char* argv[]) {
 
     hello<<<7,7>>>();
     cudaDeviceSynchronize();
